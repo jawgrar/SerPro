2e96f48b8dd76bfc0186fbab1b2918dab27d3d62
Pratik kankhara <p.kankhara@gmail.com>
2015-09-27 02:11:24 +0530

    Remove hash tag and blue menu bar

--- SerPro.Web/app/app.js
+++ SerPro.Web/app/app.js
@@ -3,52 +3,48 @@ var app = angular.module('AngularAuthApp', ['ngResource', 'ngCookies', 'ngRoute'
 
 app.config(["$routeProvider", "$locationProvider", function ($routeProvider, $locationProvider) {
 
-    $routeProvider.when("/home", {
+    $routeProvider.when("/", {
         controller: "homeController",
         templateUrl: "/app/views/home.html"
-    });
-
-    $routeProvider.when("/login", {
+    }).when("/login", {
         controller: "loginController",
         templateUrl: "/app/views/login.html"
-    });
-
-    $routeProvider.when("/signup", {
+    }).when("/signup", {
         controller: "signupController",
         templateUrl: "/app/views/signup.html"
-    });
-
-    $routeProvider.when("/orders", {
+    }).when("/orders", {
         controller: "ordersController",
         templateUrl: "/app/views/orders.html"
-    });
-
-    $routeProvider.when("/refresh", {
+    }).when('/FileUpload', {
+        templateUrl: 'app/fileUpload/photos.html',
+        controller: 'photos',
+        controllerAs: 'vm',
+        caseInsensitiveMatch: true
+    }).when('/landing', {
+        templateUrl: 'app/views/landing.html',
+        controller: 'photos',
+        controllerAs: 'vm',
+        caseInsensitiveMatch: true
+    }).when("/product", {
+        templateUrl: "app/views/product.html",
+        controller: "productController"
+    }).when("/refresh", {
         controller: "refreshController",
         templateUrl: "/app/views/refresh.html"
-    });
-
-    $routeProvider.when("/tokens", {
+    }).when("/tokens", {
         controller: "tokensManagerController",
         templateUrl: "/app/views/tokens.html"
-    });
-
-    $routeProvider.when("/associate", {
+    }).when("/associate", {
         controller: "associateController",
         templateUrl: "/app/views/associate.html"
-    });
-
-    $routeProvider.when("/homePage", {
+    }).when("/homePage", {
         controller: "productController",
         templateUrl: "/app/views/homePage.html"
     });
 
-    $routeProvider.when("/product", {
-        templateUrl: "app/views/product.html",
-        controller: "productController"
-    });
+    //$routeProvider.otherwise({ redirectTo: "/home" });
 
-    $routeProvider.otherwise({ redirectTo: "/home" });
+    $locationProvider.html5Mode(true);
 
 }]);
 
